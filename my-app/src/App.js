a06b3db5ec93b60568ecbb85e6b21b424efc7460
Prajakta <shankarprajakta.k@gmail.com>
2022-12-26 15:45:18 +0530

    first commited

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Menubar_Items from './components/Menubar_Items';
+import CheckboxDemo from './components/Checkbox';
+
+function App() {
+  return (
+    <>
+    <div>
+     <Menubar_Items/>
+     <CheckboxDemo/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/demo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const demo = () => {
+  return (
+    <div>
+      <h1>Hello git..!!</h1>
+    </div>
+  )
+}
+
+export default demo
